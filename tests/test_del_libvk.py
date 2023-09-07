bd0109eedaaeb521ebc9ca3f4448173b57477687
Hideousmon <mailtozyzhao@163.com>
2023-09-07 19:30:50 +0800

    test del libvulkan

--- tests/test_del_libvk.py
+++ tests/test_del_libvk.py
@@ -19,9 +19,9 @@ import glob
 if __name__ == "__main__":
     print(platform.system())
     if platform.system() == 'Linux':
-        print(glob.glob(r'/usr/lib/libvulkan*', recursive=True))
-        print(glob.glob(r'/usr/lib64/libvulkan*', recursive=True))
+        print(glob.glob(r'/usr/lib/*.so*', recursive=True))
+        print(glob.glob(r'/usr/lib64/*.so*', recursive=True))
 
     if platform.system() == 'Windows':
-        print(glob.glob(r'/usr/lib/libvulkan*', recursive=True))
-        print(glob.glob(r'/usr/lib64/libvulkan*', recursive=True))
+        # print(glob.glob(r'/usr/lib/libvulkan*', recursive=True))
+        # print(glob.glob(r'/usr/lib64/libvulkan*', recursive=True))
