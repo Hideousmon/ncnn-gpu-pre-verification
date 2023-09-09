12f1808f640fbbe9865f5ff3881b0b8782d0e52b
Hideousmon <mailtozyzhao@163.com>
2023-09-09 21:09:44 +0800

    test inference without libvulkan

--- tests/test_net_withoutvk.py
+++ tests/test_net_withoutvk.py
@@ -16,6 +16,7 @@ import numpy as np
 import platform
 import pytest
 import glob
+import os
 
 import ncnn
 
@@ -58,25 +59,48 @@ def test_net_without_vulkan():
         lib = glob.glob(r'/usr/local/lib/**/libvulkan*')
         lib64 = glob.glob(r'/usr/local/lib64/**/libvulkan*')
 
+    if libs != [] or libs64 != []:
+        for found_lib in libs:
+            os.remove(found_lib)
+        for found_lib in libs64:
+            os.remove(found_lib)
+        print("trying to remove the libs.")
+
+    
+    if platform.system() == 'Linux':
+        libs = glob.glob(r'/usr/lib/**/libvulkan*')
+        libs64 = glob.glob(r'/usr/lib64/**/libvulkan*')
+
+    if platform.system() == 'Windows':
+        libs = glob.glob(r'c:\Windows\System32\**\libvulkan*')
+        libs64 = glob.glob(r'c:\Windows\SysWOW64\**\libvulkan*')
         
-    # dr = ncnn.DataReaderFromEmpty()
 
-    # with ncnn.Net() as net:
-    #     ret = net.load_param("tests/test.param")
-    #     net.load_model(dr)
-    #     assert ret == 0 and len(net.blobs()) == 3 and len(net.layers()) == 3
+    if platform.system() == 'Darwin':
+        libs = glob.glob(r'/usr/local/lib/**/libvulkan*')
+        libs64 = glob.glob(r'/usr/local/lib64/**/libvulkan*')
 
-    #     input_names = net.input_names()
-    #     output_names = net.output_names()
-    #     assert len(input_names) > 0 and len(output_names) > 0
+    assert libs == [] or libs64 == []
+    print("there is no libvulkan anymore. performing inference...")
 
-    #     in_mat = ncnn.Mat((227, 227, 3))
+    dr = ncnn.DataReaderFromEmpty()
 
-    #     with net.create_extractor() as ex:
-    #         ex.input("data", in_mat)
-    #         ret, out_mat = ex.extract("output")
+    with ncnn.Net() as net:
+        ret = net.load_param("tests/test.param")
+        net.load_model(dr)
+        assert ret == 0 and len(net.blobs()) == 3 and len(net.layers()) == 3
 
-    #     assert ret == 0 and out_mat.dims == 1 and out_mat.w == 1
+        input_names = net.input_names()
+        output_names = net.output_names()
+        assert len(input_names) > 0 and len(output_names) > 0
 
-    #     net.clear()
-    #     assert len(net.blobs()) == 0 and len(net.layers()) == 0
+        in_mat = ncnn.Mat((227, 227, 3))
+
+        with net.create_extractor() as ex:
+            ex.input("data", in_mat)
+            ret, out_mat = ex.extract("output")
+
+        assert ret == 0 and out_mat.dims == 1 and out_mat.w == 1
+
+        net.clear()
+        assert len(net.blobs()) == 0 and len(net.layers()) == 0
